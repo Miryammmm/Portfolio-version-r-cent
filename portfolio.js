6d72d60752576933ee3ecd7ee784caf23fda527d
Miryammmm <mairamep2@gmail.com>
2024-11-27 10:44:59 +0000

    Delete portfolio.js

--- portfolio.js
+++
@@ -1,9 +0,0 @@
-// Récupérer les éléments
-const menuToggle = document.getElementById('menu-toggle');
-const menu = document.getElementById('menu');
-
-// Ajouter l'événement de clic sur le bouton burger
-menuToggle.addEventListener('click', () => {
-    // Toggle la classe "active" sur le menu pour l'afficher/masquer
-    menu.classList.toggle('active');
-});
